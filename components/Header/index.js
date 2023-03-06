d4fbdceb5a54484cd287db8c58c00bdffa1d023b
Luis Landa <luis.landa@posteo.de>
2023-03-06 16:29:24 +0100

    creating the header

---
+++ components/Header/index.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export default function Header() {
+  return (
+    <>
+      <StyledHeader>TORTUGA</StyledHeader>
+    </>
+  );
+}
+
+const StyledHeader = styled.h1`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 59px;
+  text-align: center;
+  border-bottom: 1px solid black;
+  z-index: 10;
+  margin: 0;
+  padding: 10px;
+  background-color: #eaeaea;
+`;
