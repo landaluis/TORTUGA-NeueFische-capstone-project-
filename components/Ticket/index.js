4c4ab88376d00988a6f588edd4c010fb344e0995
Luis Landa <luis.landa@posteo.de>
2023-03-09 14:57:23 +0100

    adding Ticket component

---
+++ components/Ticket/index.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export default function Ticket({ ticketValue, newID }) {
+  return (
+    <StyledTicket>
+      <div>
+        <StyledValue>{ticketValue}€</StyledValue>
+      </div>
+    </StyledTicket>
+  );
+}
+
+const StyledTicket = styled.div`
+  box-sizing: border-box;
+  text-align: center;
+  background: #eaeaea;
+  border: 1px solid #eaeaea;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 17px;
+  width: 340px;
+  height: 100px;
+  margin: 16px auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+`;
+
+const StyledValue = styled.h1`
+  font-size: 48px;
+  font-weight: bold;
+  margin: 0;
+`;
+
+const StyledID = styled.div`
+  font-size: 16px;
+  position: absolute;
+  top: 8px;
+  right: 8px;
+`;
