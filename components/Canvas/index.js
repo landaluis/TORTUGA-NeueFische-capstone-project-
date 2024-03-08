5b22a5400b6291d76f66559bf464e12657f7a988
Luis Landa <luislanda@Luiss-MacBook-Pro.local>
2024-03-08 14:19:00 +0100

    Fixing margin in Navigation component

--- components/Canvas/index.js
+++ components/Canvas/index.js
@@ -2,79 +2,79 @@ import styled from "styled-components";
 import { uid } from "uid";
 
 export default function Canvas({ birthday, pixels }) {
-  return (
-    <>
-      <StyledCardCanvas>
-        <PixelContainer>
-          {pixels?.map((pixel) => (
-            <Pixel
-              key={uid()}
-              style={{
-                background: `${pixel}`,
-                width: "7.5px",
-                height: "7.5px",
-                gap: 0,
-              }}
-            />
-          ))}
-        </PixelContainer>
+	return (
+		<>
+			<StyledCardCanvas>
+				<PixelContainer>
+					{pixels?.map((pixel) => (
+						<Pixel
+							key={uid()}
+							style={{
+								background: `${pixel}`,
+								width: "7.5px",
+								height: "7.5px",
+								gap: 0,
+							}}
+						/>
+					))}
+				</PixelContainer>
 
-        <Title>Birthday :</Title>
-        <Birthday>{birthday}</Birthday>
-      </StyledCardCanvas>
-    </>
-  );
+				<Title>Birthday :</Title>
+				<Birthday>{birthday}</Birthday>
+			</StyledCardCanvas>
+		</>
+	);
 }
 
 const StyledCardCanvas = styled.section`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  border: 1px solid grey;
-  background: rgba(242, 242, 242, 0.4);
-  width: 143px;
-  height: 163px;
-  margin: 0;
-  position: absolute;
-  right: 35px;
-  top: 10px;
-  border-radius: 17px;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	border: 1px solid grey;
+	background: rgba(242, 242, 242, 0.4);
+	width: 143px;
+	height: 163px;
+	margin: 0;
+	position: absolute;
+	right: 35px;
+	top: 10px;
+	border-radius: 17px;
+	box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 `;
 
 const PixelContainer = styled.div`
-  position: relative;
-  top: 8px;
-  transform: rotate(180deg);
-  height: 110px;
-  max-width: 150px;
-  display: grid;
-  aspect-ratio: 1 / 1;
-  grid-template-columns: repeat(15, 1fr);
-  grid-template-rows: repeat(15, 1fr);
-  background-color: #999;
-  border-radius: 4px;
-  overflow: hidden;
-  clip-path: ellipse(40% 46% at 52% 50%);
+	position: relative;
+	top: 8px;
+	transform: rotate(180deg);
+	height: 110px;
+	max-width: 150px;
+	display: grid;
+	aspect-ratio: 1 / 1;
+	grid-template-columns: repeat(15, 1fr);
+	grid-template-rows: repeat(15, 1fr);
+	background-color: #999;
+	border-radius: 4px;
+	overflow: hidden;
+	clip-path: ellipse(40% 46% at 52% 50%);
 `;
 
 const Pixel = styled.div`
-  width: 7.5px;
-  height: 50px;
-  z-index: 40;
+	width: 7.5px;
+	height: 50px;
+	z-index: 40;
 `;
 
 const Birthday = styled.div`
-  position: absolute;
-  text-align: center;
-  top: 127px;
-  font-size: 14px;
+	position: absolute;
+	text-align: center;
+	top: 127px;
+	font-size: 14px;
 `;
 
 const Title = styled.h5`
-  margin: 16px;
-  position: absolute;
-  text-align: center;
-  top: 110px;
-  margin: 0;
+	margin: 16px;
+	position: absolute;
+	text-align: center;
+	top: 110px;
+	margin: 0;
 `;
--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -6,71 +6,72 @@ import TicketIcon from "../../public/TicketIcon.svg";
 import CardsIcon from "../../public/CardsIcon.svg";
 import ProfileIcon from "../../public/ProfileIcon.svg";
 export default function Navigation() {
-  return (
-    <StyledList>
-      <li>
-        <Link href={"/"}>
-          <Image
-            src={CardsIcon}
-            alt="Home Page"
-            title="Home Page"
-            width="35"
-            height="35"
-          />
-        </Link>
-      </li>
-      <li>
-        <Link href={"/create-card"}>
-          <Image
-            src={PlusIcon}
-            alt="Add Card"
-            title="Add Card"
-            width="35"
-            height="35"
-          />
-        </Link>
-      </li>
-      <li>
-        <Link href={"/create-ticket"}>
-          <Image
-            src={TicketIcon}
-            alt="Create Ticket"
-            title="Create Ticket"
-            width="35"
-            height="35"
-          />
-        </Link>
-      </li>
-      <li>
-        {" "}
-        <Link href={"/profilePage"}>
-          <Image
-            src={ProfileIcon}
-            alt="Profile Page"
-            title="Profile Page"
-            width="35"
-            height="35"
-          />
-        </Link>
-      </li>
-    </StyledList>
-  );
+	return (
+		<StyledList>
+			<li>
+				<Link href={"/"}>
+					<Image
+						src={CardsIcon}
+						alt="Home Page"
+						title="Home Page"
+						width="35"
+						height="35"
+					/>
+				</Link>
+			</li>
+			<li>
+				<Link href={"/create-card"}>
+					<Image
+						src={PlusIcon}
+						alt="Add Card"
+						title="Add Card"
+						width="35"
+						height="35"
+					/>
+				</Link>
+			</li>
+			<li>
+				<Link href={"/create-ticket"}>
+					<Image
+						src={TicketIcon}
+						alt="Create Ticket"
+						title="Create Ticket"
+						width="35"
+						height="35"
+					/>
+				</Link>
+			</li>
+			<li>
+				{" "}
+				<Link href={"/profilePage"}>
+					<Image
+						src={ProfileIcon}
+						alt="Profile Page"
+						title="Profile Page"
+						width="35"
+						height="35"
+					/>
+				</Link>
+			</li>
+		</StyledList>
+	);
 }
 
 const StyledList = styled.ul`
-  display: flex;
-  justify-content: space-around;
-  width: 100%;
-  height: 58px;
-  position: fixed;
-  bottom: 0;
-  font-size: 10px;
-  padding: 3px;
-  align-items: center;
-  background-color: #faf1da;
-  z-index: 21;
+	display: flex;
+	justify-content: space-around;
+	width: 100%;
+	height: 58px;
+	position: fixed;
+	bottom: 0;
+	font-size: 10px;
+	padding: 3px;
+	align-items: center;
+	background-color: #faf1da;
+	z-index: 21;
+	margin: 0;
 
-  & > li {
-    list-style: none;
-  }
+	& > li {
+		list-style: none;
+	}
 `;
